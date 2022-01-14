58bbb05d9f1fc694f75c85d0cabdc8ef00f2674e
Robert Ing <ring@mparticle.com>
2022-01-14 15:56:51 -0500

    feat: updated project naming to GA4

--- mParticle-Google-Analytics-Firebase-GA4.podspec
+++ mParticle-Google-Analytics-Firebase-GA4.podspec
@@ -1,21 +1,21 @@
 Pod::Spec.new do |s|
-    s.name             = "mParticle-Google-Analytics-Firebase"
-    s.version          = "8.0.9"
-    s.summary          = "Google Analytics for Firebase integration for mParticle"
+    s.name             = "mParticle-Google-Analytics-Firebase-GA4"
+    s.version          = "8.1.0"
+    s.summary          = "Google Analytics 4 for Firebase integration for mParticle"
 
     s.description      = <<-DESC
-                       This is the Google Analytics for Firebase integration for mParticle.
+                       This is the Google Analytics 4 for Firebase integration for mParticle.
                        DESC
 
     s.homepage         = "https://www.mparticle.com"
     s.license          = { :type => 'Apache 2.0', :file => 'LICENSE' }
     s.author           = { "mParticle" => "support@mparticle.com" }
-    s.source           = { :git => "https://github.com/mparticle-integrations/mparticle-apple-integration-google-analytics-firebase.git", :tag => s.version.to_s }
+    s.source           = { :git => "https://github.com/mparticle-integrations/mparticle-apple-integration-google-analytics-firebase-ga4.git", :tag => s.version.to_s }
     s.social_media_url = "https://twitter.com/mparticle"
     s.static_framework = true
 
     s.ios.deployment_target = "9.0"
-    s.ios.source_files      = 'mParticle-Google-Analytics-Firebase/*.{h,m,mm}'
+    s.ios.source_files      = 'mParticle-Google-Analytics-Firebase-GA4/*.{h,m,mm}'
     s.ios.dependency 'mParticle-Apple-SDK/mParticle', '~> 8.0'
     s.ios.frameworks = 'CoreTelephony', 'SystemConfiguration'
     s.libraries = 'z'
--- mParticle-Google-Analytics-Firebase-GA4Tests/MPKitFirebaseGA4AnalyticsTests.m
+++ mParticle-Google-Analytics-Firebase-GA4Tests/MPKitFirebaseGA4AnalyticsTests.m
@@ -1,24 +1,12 @@
 #import <XCTest/XCTest.h>
-#import "MPKitFirebaseAnalytics.h"
+#import "MPKitFirebaseGA4Analytics.h"
 #import "Firebase.h"
-#import "OCMock.h"
 
-@interface NSBundle (BundleIdentifier)
--(NSString *)bundleIdentifier;
+@interface FIRApp()
++ (void)resetApps;
 @end
 
-@implementation NSBundle (BundleIdentifier)
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wobjc-protocol-method-implementation"
-- (NSString *)bundleIdentifier {
-    return @"com.mparticle.iOS-Example";
-}
-#pragma clang diagnostic pop
-@end
-
-@interface MPKitFirebaseAnalytics()
-
-@property (nonatomic, strong, readwrite) FIROptions *firebaseOptions;
+@interface MPKitFirebaseGA4Analytics()
 - (NSString *)standardizeNameOrKey:(NSString *)nameOrKey forEvent:(BOOL)forEvent;
 - (NSString *)getEventNameForCommerceEvent:(MPCommerceEvent *)commerceEvent parameters:(NSDictionary<NSString *, id> *)parameters;
 - (NSDictionary<NSString *, id> *)getParameterForCommerceEvent:(MPCommerceEvent *)commerceEvent;
@@ -30,81 +18,66 @@
 @implementation mParticle_Firebase_AnalyticsTests
 
 - (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    NSString *bundlePath = [[NSBundle bundleForClass:[self class]] resourcePath];
+    NSString *filePath = [bundlePath stringByAppendingPathComponent:@"GoogleService-Info.plist"];
+    FIROptions *options = [[FIROptions alloc] initWithContentsOfFile:filePath];
+    [FIRApp configureWithOptions:options];
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [FIRApp resetApps];
 }
 
 - (void)testStarted {
-    MPKitFirebaseAnalytics *exampleKit = [[MPKitFirebaseAnalytics alloc] init];
-    [exampleKit didFinishLaunchingWithConfiguration:@{kMPFIRGoogleAppIDKey:@"1:338209672096:ios:57235e7ff821ba85", kMPFIRSenderIDKey:@"338209672096", kMPFIRAPIKey: @"AIzaSyDVH6Lxu4QvIWheB14FChPIdI6FiCi8PXY", kMPFIRProjectIDKey: @"mparticle-integration-test"}];
+    MPKitFirebaseGA4Analytics *exampleKit = [[MPKitFirebaseGA4Analytics alloc] init];
+    [exampleKit didFinishLaunchingWithConfiguration:@{}];
+    
     XCTAssertTrue(exampleKit.started);
 }
 
-- (void)testFIRApp {
-    id mockFIRApp = OCMClassMock([FIRApp class]);
-    FIROptions *options = [[FIROptions alloc] initWithGoogleAppID:@"1:338209672096:ios:57235e7ff821ba85" GCMSenderID:@"338209672096"];
-    
-    
-    [[mockFIRApp expect] configureWithOptions:options];
-    
-    [FIRApp configureWithOptions:options];
-    
-    [mockFIRApp verify];
-    [mockFIRApp stopMocking];
-}
-
 - (void)testLogCommerceEvent {
-    MPKitFirebaseAnalytics *exampleKit = [[MPKitFirebaseAnalytics alloc] init];
-    [exampleKit didFinishLaunchingWithConfiguration:@{kMPFIRGoogleAppIDKey:@"1:338209672096:ios:57235e7ff821ba85", kMPFIRSenderIDKey:@"338209672096", kMPFIRAPIKey: @"AIzaSyDVH6Lxu4QvIWheB14FChPIdI6FiCi8PXY", kMPFIRProjectIDKey: @"mparticle-integration-test"}];
-    
+    MPKitFirebaseGA4Analytics *exampleKit = [[MPKitFirebaseGA4Analytics alloc] init];
+    [exampleKit didFinishLaunchingWithConfiguration:@{}];
+
     MPCommerceEvent *event = [[MPCommerceEvent alloc] initWithAction:MPCommerceEventActionCheckoutOptions];
-    
     MPKitExecStatus *execStatus = [exampleKit logBaseEvent:event];
-    
     XCTAssertTrue(execStatus.success);
-    XCTAssertTrue(exampleKit.firebaseOptions);
 }
 
 - (void)testLogCommerceEventPurchase {
-    MPKitFirebaseAnalytics *exampleKit = [[MPKitFirebaseAnalytics alloc] init];
-    [exampleKit didFinishLaunchingWithConfiguration:@{kMPFIRGoogleAppIDKey:@"1:338209672096:ios:57235e7ff821ba85", kMPFIRSenderIDKey:@"338209672096", kMPFIRAPIKey: @"AIzaSyDVH6Lxu4QvIWheB14FChPIdI6FiCi8PXY", kMPFIRProjectIDKey: @"mparticle-integration-test"}];
-    
+    MPKitFirebaseGA4Analytics *exampleKit = [[MPKitFirebaseGA4Analytics alloc] init];
+    [exampleKit didFinishLaunchingWithConfiguration:@{}];
+
     MPProduct *product = [[MPProduct alloc] initWithName:@"Tardis" sku:@"9who" quantity:@1 price:@42.0];
-    
     MPCommerceEvent *event = [[MPCommerceEvent alloc] initWithAction:MPCommerceEventActionPurchase product:product];
-    
     MPKitExecStatus *execStatus = [exampleKit logBaseEvent:event];
-    
+
     XCTAssertTrue(execStatus.success);
 }
 
 - (void)testLogEvent {
-    MPKitFirebaseAnalytics *exampleKit = [[MPKitFirebaseAnalytics alloc] init];
-    [exampleKit didFinishLaunchingWithConfiguration:@{kMPFIRGoogleAppIDKey:@"1:338209672096:ios:57235e7ff821ba85", kMPFIRSenderIDKey:@"338209672096", kMPFIRAPIKey: @"AIzaSyDVH6Lxu4QvIWheB14FChPIdI6FiCi8PXY", kMPFIRProjectIDKey: @"mparticle-integration-test"}];
-    
+    MPKitFirebaseGA4Analytics *exampleKit = [[MPKitFirebaseGA4Analytics alloc] init];
+    [exampleKit didFinishLaunchingWithConfiguration:@{}];
+
     MPEvent *event = [[MPEvent alloc] initWithName:@"example" type:MPEventTypeOther];
-    
     MPKitExecStatus *execStatus = [exampleKit logBaseEvent:event];
-    
+
     XCTAssertTrue(execStatus.success);
 }
 
 - (void)testLogEventWithNilEvent {
-    MPKitFirebaseAnalytics *exampleKit = [[MPKitFirebaseAnalytics alloc] init];
-    [exampleKit didFinishLaunchingWithConfiguration:@{kMPFIRGoogleAppIDKey:@"1:338209672096:ios:57235e7ff821ba85", kMPFIRSenderIDKey:@"338209672096", kMPFIRAPIKey: @"AIzaSyDVH6Lxu4QvIWheB14FChPIdI6FiCi8PXY", kMPFIRProjectIDKey: @"mparticle-integration-test"}];
-    
-    MPEvent *event;
-    
+    MPKitFirebaseGA4Analytics *exampleKit = [[MPKitFirebaseGA4Analytics alloc] init];
+    [exampleKit didFinishLaunchingWithConfiguration:@{}];
+
+    MPEvent *event = nil;
     MPKitExecStatus *execStatus = [exampleKit logBaseEvent:event];
-    
+
     XCTAssertFalse(execStatus.success);
 }
 
 - (void)testSanitization {
-    MPKitFirebaseAnalytics *exampleKit = [[MPKitFirebaseAnalytics alloc] init];
+    MPKitFirebaseGA4Analytics *exampleKit = [[MPKitFirebaseGA4Analytics alloc] init];
+    
     XCTAssertEqualObjects([exampleKit standardizeNameOrKey:@"event name" forEvent:YES], @"event_name");
     XCTAssertEqualObjects([exampleKit standardizeNameOrKey:@"event_name " forEvent:YES], @"event_name_");
     XCTAssertEqualObjects([exampleKit standardizeNameOrKey:@"event  name " forEvent:YES], @"event_name_");
@@ -116,27 +89,27 @@
 }
 
 - (void)testCommerceEventCheckoutOptions {
-    MPKitFirebaseAnalytics *exampleKit = [[MPKitFirebaseAnalytics alloc] init];
-    [exampleKit didFinishLaunchingWithConfiguration:@{kMPFIRGoogleAppIDKey:@"1:338209672096:ios:57235e7ff821ba85", kMPFIRSenderIDKey:@"338209672096", kMPFIRAPIKey: @"AIzaSyDVH6Lxu4QvIWheB14FChPIdI6FiCi8PXY", kMPFIRProjectIDKey: @"mparticle-integration-test"}];
-    
+    MPKitFirebaseGA4Analytics *exampleKit = [[MPKitFirebaseGA4Analytics alloc] init];
+    [exampleKit didFinishLaunchingWithConfiguration:@{}];
+
     // Test fallback when not using GA4
     MPCommerceEvent *event = [[MPCommerceEvent alloc] initWithAction:MPCommerceEventActionCheckoutOptions];
     NSDictionary<NSString *, id> *parameters = [exampleKit getParameterForCommerceEvent:event];
     NSString *eventName = [exampleKit getEventNameForCommerceEvent:event parameters:parameters];
     XCTAssertEqualObjects(kFIREventSetCheckoutOption, eventName);
-    
+
     // Test kFIREventAddShippingInfo
     event = [[MPCommerceEvent alloc] initWithAction:MPCommerceEventActionCheckoutOptions];
     [event addCustomFlag:kFIREventAddShippingInfo withKey:kMPFIRGA4CommerceEventType];
     eventName = [exampleKit getEventNameForCommerceEvent:event parameters:parameters];
     XCTAssertEqualObjects(kFIREventAddShippingInfo, eventName);
-    
+
     // Test kFIREventAddPaymentInfo
     event = [[MPCommerceEvent alloc] initWithAction:MPCommerceEventActionCheckoutOptions];
     [event addCustomFlag:kFIREventAddPaymentInfo withKey:kMPFIRGA4CommerceEventType];
     eventName = [exampleKit getEventNameForCommerceEvent:event parameters:parameters];
     XCTAssertEqualObjects(kFIREventAddPaymentInfo, eventName);
-    
+
     // Test both (defaults to kFIREventAddShippingInfo)
     event = [[MPCommerceEvent alloc] initWithAction:MPCommerceEventActionCheckoutOptions];
     [event addCustomFlags:@[kFIREventAddShippingInfo, kFIREventAddPaymentInfo] withKey:kMPFIRGA4CommerceEventType];
