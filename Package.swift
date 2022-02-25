f77caaf9bcc1b38ac65cbd16fd6edf0a4f19ee36
Ben Baron <bbaron@mparticle.com>
2022-02-25 14:10:37 -0500

    feat: add SPM support
    Also removed an unnecessary header that was causing module import problems.
    
    Tested in a matrix of 4 test projects using both Obj-C and Swift and CocoaPods and SPM.

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "mParticle-Google-Analytics-Firebase-GA4",
+    platforms: [ .iOS(.v11) ],
+    products: [
+        .library(
+            name: "mParticle-Google-Analytics-Firebase-GA4",
+            targets: ["mParticle-Google-Analytics-Firebase-GA4"]),
+    ],
+    dependencies: [
+      .package(name: "mParticle-Apple-SDK",
+               url: "https://github.com/mParticle/mparticle-apple-sdk",
+               .upToNextMajor(from: "8.0.0")),
+      .package(name: "Firebase",
+               url: "https://github.com/firebase/firebase-ios-sdk.git",
+               .upToNextMajor(from: "8.10.0")),
+    ],
+    targets: [
+        .target(
+            name: "mParticle-Google-Analytics-Firebase-GA4",
+            dependencies: [
+              .byName(name: "mParticle-Apple-SDK"),
+              .product(name: "FirebaseAnalytics", package: "Firebase"),
+            ],
+            path: "mParticle-Google-Analytics-Firebase-GA4",
+            exclude: ["Info.plist"],
+            publicHeadersPath: "."),
+    ]
+)
--- mParticle-Google-Analytics-Firebase-GA4/mParticle-Firebase-Analytics.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-FOUNDATION_EXPORT double mParticle_Firebase_AnalyticsVersionNumber;
-
-FOUNDATION_EXPORT const unsigned char mParticle_Firebase_AnalyticsVersionString[];
-
-#import <mParticle_Google_Analytics_Firebase/MPKitFirebaseAnalytics.h>
