2dd14cdddd4138ab5cdf412d6bc060b89933179d
Brandon Stalnaker <33703490+BrandonStalnaker@users.noreply.github.com>
2023-05-05 11:37:35 -0400

    feat: Support Max Parameters and Properties (#18)

--- mParticle-Google-Analytics-Firebase-GA4Tests/MPKitFirebaseGA4AnalyticsTests.m
+++ mParticle-Google-Analytics-Firebase-GA4Tests/MPKitFirebaseGA4AnalyticsTests.m
@@ -40,7 +40,7 @@
 - (void)testLogCommerceEvent {
     MPKitFirebaseGA4Analytics *exampleKit = [[MPKitFirebaseGA4Analytics alloc] init];
     [exampleKit didFinishLaunchingWithConfiguration:@{}];
-
+    
     MPCommerceEvent *event = [[MPCommerceEvent alloc] initWithAction:MPCommerceEventActionCheckoutOptions];
     MPKitExecStatus *execStatus = [exampleKit logBaseEvent:event];
     XCTAssertTrue(execStatus.success);
@@ -49,61 +49,61 @@
 - (void)testLogCommerceEventPurchase {
     MPKitFirebaseGA4Analytics *exampleKit = [[MPKitFirebaseGA4Analytics alloc] init];
     [exampleKit didFinishLaunchingWithConfiguration:@{}];
-
+    
     MPProduct *product = [[MPProduct alloc] initWithName:@"Tardis" sku:@"9who" quantity:@1 price:@42.0];
     MPCommerceEvent *event = [[MPCommerceEvent alloc] initWithAction:MPCommerceEventActionPurchase product:product];
     MPKitExecStatus *execStatus = [exampleKit logBaseEvent:event];
-
+    
     XCTAssertTrue(execStatus.success);
 }
 
 - (void)testLogCommerceEventImpression {
     MPKitFirebaseGA4Analytics *exampleKit = [[MPKitFirebaseGA4Analytics alloc] init];
     [exampleKit didFinishLaunchingWithConfiguration:@{}];
-
+    
     MPProduct *product = [[MPProduct alloc] initWithName:@"Tardis" sku:@"9who" quantity:@1 price:@42.0];
     MPCommerceEvent *event = [[MPCommerceEvent alloc] initWithImpressionName:@"suggested products list" product:product];
     MPKitExecStatus *execStatus = [exampleKit logBaseEvent:event];
-
+    
     XCTAssertTrue(execStatus.success);
 }
 
 - (void)testLogCommerceEventPromotion {
     MPKitFirebaseGA4Analytics *exampleKit = [[MPKitFirebaseGA4Analytics alloc] init];
     [exampleKit didFinishLaunchingWithConfiguration:@{}];
-
+    
     MPPromotion *promotion = [[MPPromotion alloc] init];
     promotion.promotionId = @"my_promo_1";
     promotion.creative = @"sale_banner_1";
     promotion.name = @"App-wide 50% off sale";
     promotion.position = @"dashboard_bottom";
-
+    
     MPPromotionContainer *container = [[MPPromotionContainer alloc] initWithAction:MPPromotionActionView promotion:promotion];
-
+    
     MPCommerceEvent *event = [[MPCommerceEvent alloc] initWithPromotionContainer:container];
     
     MPKitExecStatus *execStatus = [exampleKit logBaseEvent:event];
-
+    
     XCTAssertTrue(execStatus.success);
 }
 
 - (void)testLogEvent {
     MPKitFirebaseGA4Analytics *exampleKit = [[MPKitFirebaseGA4Analytics alloc] init];
     [exampleKit didFinishLaunchingWithConfiguration:@{}];
-
+    
     MPEvent *event = [[MPEvent alloc] initWithName:@"example" type:MPEventTypeOther];
     MPKitExecStatus *execStatus = [exampleKit logBaseEvent:event];
-
+    
     XCTAssertTrue(execStatus.success);
 }
 
 - (void)testLogEventWithNilEvent {
     MPKitFirebaseGA4Analytics *exampleKit = [[MPKitFirebaseGA4Analytics alloc] init];
     [exampleKit didFinishLaunchingWithConfiguration:@{}];
-
+    
     MPEvent *event = nil;
     MPKitExecStatus *execStatus = [exampleKit logBaseEvent:event];
-
+    
     XCTAssertFalse(execStatus.success);
 }
 
@@ -130,8 +130,8 @@
     XCTAssertEqualObjects([exampleKit standardizeNameOrKey:@"event! - ?name " forEvent:NO], @"event! - ?name ");
     XCTAssertEqualObjects([exampleKit standardizeNameOrKey:@"!@#$%^&*()_+=[]{}|'\"?>" forEvent:NO], @"!@#$%^&*()_+=[]{}|'\"?>");
     XCTAssertEqualObjects([exampleKit standardizeNameOrKey:@"   event_name" forEvent:NO], @"   event_name");
-
-
+    
+    
     
     NSArray *badStarts = @[@"!@#$%^&*()_+=[]{}|'\"?><:;event_name",
                            @"_event_name",
@@ -155,6 +155,135 @@
     for (NSString *emptyString in emptyStrings) {
         XCTAssertEqualObjects([exampleKit standardizeNameOrKey:emptyString forEvent:YES], @"invalid_ga4_key");
     }
+}
+
+- (void)testSanitizationMax {
+    MPKitFirebaseGA4Analytics *exampleKit = [[MPKitFirebaseGA4Analytics alloc] init];
+    [exampleKit didFinishLaunchingWithConfiguration:@{}];
+    
+    NSDictionary *testAttributes = @{ @"test1": @"parameter",
+                                      @"test2": @"parameter",
+                                      @"test3": @"parameter",
+                                      @"test4": @"parameter",
+                                      @"test5": @"parameter",
+                                      @"test6": @"parameter",
+                                      @"test7": @"parameter",
+                                      @"test8": @"parameter",
+                                      @"test9": @"parameter",
+                                      @"test10": @"parameter",
+                                      @"test11": @"parameter",
+                                      @"test12": @"parameter",
+                                      @"test13": @"parameter",
+                                      @"test14": @"parameter",
+                                      @"test15": @"parameter",
+                                      @"test16": @"parameter",
+                                      @"test17": @"parameter",
+                                      @"test18": @"parameter",
+                                      @"test19": @"parameter",
+                                      @"test20": @"parameter",
+                                      @"test21": @"parameter",
+                                      @"test22": @"parameter",
+                                      @"test23": @"parameter",
+                                      @"test24": @"parameter"
+          };
+    NSDictionary *testFinalAttributes = @{ @"test1": @"parameter",
+                                           @"test2": @"parameter",
+                                           @"test3": @"parameter",
+                                           @"test4": @"parameter",
+                                           @"test5": @"parameter",
+                                           @"test6": @"parameter",
+                                           @"test7": @"parameter",
+                                           @"test8": @"parameter",
+                                           @"test9": @"parameter",
+                                           @"test10": @"parameter",
+                                           @"test11": @"parameter",
+                                           @"test12": @"parameter",
+                                           @"test13": @"parameter",
+                                           @"test14": @"parameter",
+                                           @"test15": @"parameter",
+                                           @"test16": @"parameter",
+                                           @"test17": @"parameter",
+                                           @"test18": @"parameter",
+                                           @"test19": @"parameter",
+                                           @"test20": @"parameter",
+                                           @"test21": @"parameter",
+                                           @"test22": @"parameter",
+                                           @"test23": @"parameter",
+                                           @"test24": @"parameter",
+                                           @"currency": @"USD"
+          };
+    
+    MPCommerceEvent *event = [[MPCommerceEvent alloc] initWithAction:MPCommerceEventActionCheckoutOptions];
+    event.customAttributes = testAttributes;
+    
+    NSDictionary<NSString *, id> *parameters = [exampleKit getParameterForCommerceEvent:event];
+    XCTAssertEqual([parameters count], 25);
+    XCTAssertEqualObjects(parameters, testFinalAttributes);
+    
+    NSDictionary *testExcessiveAttributes = @{ @"test1": @"parameter",
+                                               @"test2": @"parameter",
+                                               @"test3": @"parameter",
+                                               @"test4": @"parameter",
+                                               @"test5": @"parameter",
+                                               @"test6": @"parameter",
+                                               @"test7": @"parameter",
+                                               @"test8": @"parameter",
+                                               @"test9": @"parameter",
+                                               @"test10": @"parameter",
+                                               @"test11": @"parameter",
+                                               @"test12": @"parameter",
+                                               @"test13": @"parameter",
+                                               @"test14": @"parameter",
+                                               @"test15": @"parameter",
+                                               @"test16": @"parameter",
+                                               @"test17": @"parameter",
+                                               @"test18": @"parameter",
+                                               @"test19": @"parameter",
+                                               @"test20": @"parameter",
+                                               @"test21": @"parameter",
+                                               @"test22": @"parameter",
+                                               @"test23": @"parameter",
+                                               @"test24": @"parameter",
+                                               @"z1": @"parameter",
+                                               @"z2": @"parameter",
+                                               @"z3": @"parameter",
+                                               @"z4": @"parameter",
+                                               @"z5": @"parameter"
+          };
+    event.customAttributes = testExcessiveAttributes;
+    
+    parameters = [exampleKit getParameterForCommerceEvent:event];
+    XCTAssertEqual([parameters count], 25);
+    XCTAssertEqualObjects(parameters, testFinalAttributes);
+
+
+    MPKitExecStatus *execStatus = [exampleKit logBaseEvent:event];
+    XCTAssertTrue(execStatus.success);
+    
+    MPProduct *product1 = [[MPProduct alloc] initWithName:@"William Hartnell" sku:@"1who" quantity:@1 price:@42.0];
+    MPProduct *product2 = [[MPProduct alloc] initWithName:@"Patrick Troughton" sku:@"2who" quantity:@1 price:@42.0];
+    MPProduct *product3 = [[MPProduct alloc] initWithName:@"Jon Pertwee" sku:@"3who" quantity:@1 price:@42.0];
+    MPProduct *product4 = [[MPProduct alloc] initWithName:@"Tom Baker" sku:@"4who" quantity:@1 price:@42.0];
+    MPProduct *product5 = [[MPProduct alloc] initWithName:@"Peter Davison" sku:@"5who" quantity:@1 price:@42.0];
+    MPProduct *product6 = [[MPProduct alloc] initWithName:@"Colin Baker" sku:@"6who" quantity:@1 price:@42.0];
+    MPProduct *product7 = [[MPProduct alloc] initWithName:@"Sylvester McCoy" sku:@"7who" quantity:@1 price:@42.0];
+    MPProduct *product8 = [[MPProduct alloc] initWithName:@"Paul McGann" sku:@"8who" quantity:@1 price:@42.0];
+    MPProduct *product9 = [[MPProduct alloc] initWithName:@"Christopher Eccleston" sku:@"9who" quantity:@1 price:@42.0];
+    MPProduct *product10 = [[MPProduct alloc] initWithName:@"David Tennant" sku:@"10who" quantity:@1 price:@42.0];
+    MPProduct *product11 = [[MPProduct alloc] initWithName:@"Matt Smith" sku:@"11who" quantity:@1 price:@42.0];
+    MPProduct *product12 = [[MPProduct alloc] initWithName:@"Peter Capaldi" sku:@"12who" quantity:@1 price:@42.0];
+    MPProduct *product13 = [[MPProduct alloc] initWithName:@"Jodie Whittaker" sku:@"13who" quantity:@1 price:@42.0];
+
+    MPCommerceEvent *purchaseEvent = [[MPCommerceEvent alloc] initWithAction:MPCommerceEventActionPurchase];
+    purchaseEvent.products = @[product1, product2, product3, product4, product5, product6, product7, product8, product9, product10, product11, product12, product13];
+    
+    parameters = [exampleKit getParameterForCommerceEvent:purchaseEvent];
+    XCTAssertEqual([parameters count], 2);
+    XCTAssertEqual([parameters[@"items"] count], 13);
+    XCTAssertTrue([parameters[@"items"][0] count] <= 10);
+
+    execStatus = [exampleKit logBaseEvent:purchaseEvent];
+    XCTAssertTrue(execStatus.success);
 }
 
 - (void)testCommerceEventCheckoutOptions {
