98405a8c516624eb1af91bd2e36240dd6c4a385a
BrandonStalnaker <bstalnaker@mparticle.com>
2023-12-06 10:54:19 -0500

    ensure functionality on SPM and cocaopods

--- mParticle-Google-Analytics-Firebase-GA4/MPKitFirebaseGA4Analytics.h
+++ mParticle-Google-Analytics-Firebase-GA4/MPKitFirebaseGA4Analytics.h
@@ -1,10 +1,13 @@
 #import <Foundation/Foundation.h>
-#if defined(__has_include) && __has_include(<mParticle_Apple_SDK/mParticle-Apple-SDK-umbrella.h>)
-    #import <mParticle_Apple_SDK/mParticle-Apple-SDK-umbrella.h>
-#elif defined(__has_include) && __has_include(<mParticle_Apple_SDK_NoLocation/mParticle-Apple-SDK-umbrella.h>)
-    #import <mParticle_Apple_SDK_NoLocation/mParticle-Apple-SDK-umbrella.h>
+#if defined(__has_include) && __has_include(<mParticle_Apple_SDK/mParticle.h>)
+    #import <mParticle_Apple_SDK/mParticle.h>
+    #import <mParticle_Apple_SDK/mParticle_Apple_SDK-Swift.h>
+#elif defined(__has_include) && __has_include(<mParticle_Apple_SDK_NoLocation/mParticle.h>)
+    #import <mParticle_Apple_SDK_NoLocation/mParticle.h>
+    #import <mParticle_Apple_SDK_NoLocation/mParticle_Apple_SDK-Swift.h>
 #else
-    #import "mParticle-Apple-SDK-umbrella.h"
+    #import "mParticle.h"
+    #import "mParticle_Apple_SDK-Swift.h"
 #endif
 
 @interface MPKitFirebaseGA4Analytics : NSObject <MPKitProtocol>
